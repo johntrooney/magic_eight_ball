acb709641646479dcc5ed3c994d82a3c6ed48553
johntrooney <100171856+johntrooney@users.noreply.github.com>
2022-02-27 21:54:09 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+var userName = 'Sandra';
+console.log(`Hello, ${userName}!`);
+
+var userQuestion = 'Will I get invited to the prom?';
+console.log(`${userQuestion}`);
+
+var randomNumber = Math.floor(Math.random()* 8);
+
+var eightBall = '';
+if (randomNumber === 0) {
+    console.log(eightBall = 'BACK OFF DUDE!');
+}else if (randomNumber === 1){
+    console.log(eightBall = 'Is that a bus I hear?');
+}else if (randomNumber === 2){
+    console.log(eightBall = 'You are going to prison');
+}else if (randomNumber === 3){
+    console.log(eightBall = 'Prison will be a great new home for you');
+}else if (randomNumber === 4){
+    console.log(eightBall = 'You will gain like 40 pounds over the next few months and get arrested for that scam you have been working on.');
+}else if (randomNumber === 5){
+    console.log(eightBall = 'Very pretentious question to ask');
+}else if (randomNumber === 6){
+    console.log(eightball = 'HOW DARE YOU AS A QUESTION LIKE THAT');
+}else if (randomNumber === 7){
+    console.log(eightball = 'You are an actual piece of trash');
+}else if (randomNumber === 8){
+    console.log(eightball = 'All signs point to you getting hit by a bus');
+}
